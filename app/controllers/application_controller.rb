83527f714143c5fb605505e2cb3e0d3e74df20c6
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-04-05 23:59:40 +0300

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,10 @@
 class ApplicationController < ActionController::API
-  
-  before_action :authorized_user
   include ActionController::Cookies
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
-
-  # make current user actions
-  def user
-    @user ||= User.find_by(id: session[:user_id])
-  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
  end
+  
 
-  # make authorized current user action
-  def authorized_user
-    render json: { error: "Not authorized" }, status: :unauthorized unless user
-  end
-
-  private
-
-  def render_not_found(resp)
-    render json: { error: "#{resp.model} not found" }, status: :not_found
-  end
-
-  def render_invalid(resp)
-    render json: { errors: resp.record.errors.full_messages }, status: :unprocessable_entity
-  end
 end
+
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,12 @@
 class SessionsController < ApplicationController
-  skip_before_action :authorized_user, only: :create
-
+  
   def create
-    user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to car_rentals_path
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
     else
-      flash.now[:danger] = 'Invalid username/password combination'
-      render 'new'
+      render json: { errors: ['Invalid username or password'] }, status: :unprocessable_entity
     end
   end
 
@@ -17,8 +15,9 @@ class SessionsController < ApplicationController
     head :no_content
   end
 
+  private
+
   def session_params
     params.permit(:username, :password)
   end
-
 end
