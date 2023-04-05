dc9de27cb68291442bc2c31d58c8e06b7710c51a
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-04-06 00:06:30 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorized_user
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
- end
-  
+  end
 
+  def authorized_user
+    unless current_user
+      render json: { error: "You are not authorized to access this page." }, status: :unauthorized
+    end
+  end
 end
-
