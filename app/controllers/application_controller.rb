d7d00487dc0be2557f2ea466437c3d53d8f78a8a
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-04-05 12:03:21 +0300

    updating deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
-end
+    protect_from_forgery with: :exception
+    
+    include ActionController::Cookies
+    
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+  end
+  
