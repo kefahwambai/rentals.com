d8a718f2b45a7032f044658a4d29333506dbd432
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-04-06 19:46:01 +0300

    update d2-1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
-  before_action :authorized_user, only: [:create]
+  
+  skip_before_action :authorized_user, only: :create
 
   def create
     @user = User.find_by(username: session_params[:username])
