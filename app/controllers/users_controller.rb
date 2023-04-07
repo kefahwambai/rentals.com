cb30815b04fe241e8fadc08033603cf726660020
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-04-07 22:26:44 +0300

    reupdated params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.permit(:username, :email, :password, :password_confirmation)
   end
 
   def render_not_found_response
